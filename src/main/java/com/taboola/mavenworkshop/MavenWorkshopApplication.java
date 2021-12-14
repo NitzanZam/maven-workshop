b6ff3fa9aeb5cc776b6687887545c99aa0613fe4
Nitzan Zamir <nitzan.z@taboola.com>
2021-12-14 17:19:25 +0200

    create spring boot server

---
+++ src/main/java/com/taboola/mavenworkshop/MavenWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.taboola.mavenworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taboola/mavenworkshop/MavenWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taboola.mavenworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
