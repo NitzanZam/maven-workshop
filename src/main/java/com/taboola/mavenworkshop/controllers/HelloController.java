27723234c495e22a33a4b4918aa52a1242a10c35
Nitzan Zamir <nitzan.z@taboola.com>
2022-08-10 14:42:30 +0300

    prepare for bootcamp

--- src/main/java/com/taboola/mavenworkshop/controllers/HelloController.java
+++ src/main/java/com/taboola/mavenworkshop/controllers/HelloController.java
@@ -1,12 +1,8 @@
 package com.taboola.mavenworkshop.controllers;
 
-import java.util.HashSet;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.google.common.collect.Sets;
 
 @RestController
 @RequestMapping(value = "maven/hello")
@@ -14,7 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        HashSet<String> names = Sets.newHashSet("Alon", "Roni", "Noga");
-        return "Hello " + names;
+        return "Hello Campers!";
     }
 }
